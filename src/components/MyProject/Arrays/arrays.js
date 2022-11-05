da119397e85805cc4ed5a4437f7fceb1f6a93f1f
valiant.ocampo14 <valiant.ocampo14@gmail.com>
2022-11-05 13:48:07 +0300

    asdas

---
+++ src/components/MyProject/Arrays/arrays.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Col,Row,Card,Button} from 'react-bootstrap'
+
+const Arrays = () => {
+    const fruits = ['Banana', 'Apple', 'Kiwi']
+    return (
+        <>
+        <Card >
+            <Card.Header as="h5" className="btnTitle">Array Manipulation</Card.Header>
+            <Card.Body className="bg-profile">
+            </Card.Body>
+        </Card>
+        </>
+    )
+}
+
+export default Arrays
---
+++ src/components/MyProject/binarySearch/search.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Col,Row,Card,Button} from 'react-bootstrap'
+
+const Search = () => {
+    const fruits = ['Banana', 'Apple', 'Kiwi']
+    return (
+        <>
+        <Card >
+            <Card.Header as="h5" className="btnTitle">Binary Search</Card.Header>
+            <Card.Body className="bg-profile">
+            </Card.Body>
+        </Card>
+        </>
+    )
+}
+
+export default Search
